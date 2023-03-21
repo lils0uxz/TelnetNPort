671121cbdf9ad2c53c9f9f5888419179e535b015
Lil_s0uxz <Lil_s0uxz@WIN-5BBPU35BSRI>
2023-03-21 17:25:23 +0700

    Add project files.

---
+++ AppRestart/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace AppRestart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxIP = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTelnet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbxIP
+            // 
+            this.cbxIP.FormattingEnabled = true;
+            this.cbxIP.Items.AddRange(new object[] {
+            "192.168.3.37",
+            "192.168.3.34"});
+            this.cbxIP.Location = new System.Drawing.Point(15, 33);
+            this.cbxIP.Name = "cbxIP";
+            this.cbxIP.Size = new System.Drawing.Size(121, 21);
+            this.cbxIP.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(201, 33);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(198, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port";
+            // 
+            // btnTelnet
+            // 
+            this.btnTelnet.Location = new System.Drawing.Point(370, 9);
+            this.btnTelnet.Name = "btnTelnet";
+            this.btnTelnet.Size = new System.Drawing.Size(75, 48);
+            this.btnTelnet.TabIndex = 4;
+            this.btnTelnet.Text = "TELNET";
+            this.btnTelnet.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 67);
+            this.Controls.Add(this.btnTelnet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxIP);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RestartNPort";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ComboBox cbxIP;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTelnet;
+    }
+}
+
---
+++ AppRestart/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace AppRestart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a new telnet connection to hostname "192.168.3.37" on port "23"
+            TelnetConnection tc = new TelnetConnection("192.168.3.37", 23);
+
+            //login with password "moxa", using a timeout of 100ms, and show server output
+            string s = tc.Login( "moxa" + Environment.NewLine, 1000);
+             
+            Console.Write(s);
+
+            // server output should end with "$" or ">", otherwise the connection failed
+            string prompt = s.TrimEnd();
+            prompt = s.Substring(prompt.Length - 1, 1);
+            if (prompt != "$" && prompt != ">" && prompt != ":")
+                throw new Exception("Connection failed");
+
+            prompt = "";
+            tc.WriteLine("s" + Environment.NewLine);
+            Thread.Sleep(500);
+            tc.WriteLine("y" + Environment.NewLine);
+
+            // while connected
+            //while (tc.IsConnected && prompt.Trim() != "exit")
+            //    {
+            //        // display server output
+            //        Console.Write(tc.Read());
+            //    // send client input to server
+            //    prompt = Console.ReadLine();
+            //        tc.WriteLine(prompt);
+
+            //        // display server output
+            //        Console.Write(tc.Read());
+            //    }
+            //    Console.WriteLine("***DISCONNECTED");
+            //    Console.ReadLine();
+        }
+    }
+}
